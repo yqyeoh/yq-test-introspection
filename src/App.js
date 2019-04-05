a871e172126b31413a9c46d943c3d62098be814a
jeromelimzw <jerome.lim.zw@gmail.com>
2019-04-05 21:43:28 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Plot from "react-plotly.js";
+import "./App.css";
 
 class App extends Component {
+  getrandom = (num, mul) => {
+    const value = [];
+    for (let i = 0; i <= num; i++) {
+      const rand = Math.random() * mul;
+      value.push(rand);
+    }
+    return value;
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Plot
+        data={[
+          {
+            r: this.getrandom(62, 3),
+            t: this.getrandom(62, 250),
+            mode: "markers",
+            name: "Trial 5",
+            marker: {
+              color: "rgb(0,0,0)",
+              size: 10,
+              line: { color: "red" },
+              opacity: 1
+            },
+            type: "scatterpolar",
+            subplot: "polar"
+          },
+          {
+            r: this.getrandom(62, 3),
+            t: this.getrandom(62, 250),
+            mode: "markers",
+            name: "Trial 6",
+            marker: {
+              color: "rgb(255,0,0)",
+              size: 10,
+              line: { color: "red" },
+              opacity: 1
+            },
+            type: "scatterpolar",
+            subplot: "polar"
+          }
+        ]}
+        layout={{
+          polar: {
+            sector: [45, 90],
+            domain: {
+              x: [0, 0.4],
+              y: [0, 1]
+            }
+          },
+          showlegend: false,
+          title: "Hobbs-Pearson Trials",
+          font: { size: 15 },
+          plot_bgcolor: "rgb(223, 223, 223)",
+          angularaxis: { tickcolor: "rgb(253,253,253)" }
+        }}
+      />
     );
   }
 }
