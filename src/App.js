42ad42143a6e27e816edbcd72063210f24c3eaa2
huitian <tanhuitian@gmail.com>
2019-04-09 17:56:12 +0800

    Add test to App route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import Routes from "./Routes";
 import NavBar from "./components/NavBar/NavBar";
-
+import { BrowserRouter } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <NavBar />
-        <Routes />
-      </React.Fragment>
+      <BrowserRouter>
+        <React.Fragment>
+          <NavBar />
+          <Routes />
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
